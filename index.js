92b2b3286bc7ace880c519dedd51425d2bd19585
Maks Lyashuk <m_lyashuk@ukr.net>
2015-10-29 11:58:56 +0200

    Code style update. Add missing semicolons. Reformat with 4 spaces

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 'use strict';
 
-var request     = require('nets')
-  , queryString = require('querystring')
+var
+    request     = require('nets'),
+    queryString = require('querystring');
 
 var constructYoutubeApiUrl = function (key, endpoint, params) {
-  var baseUrl = ['https://www.googleapis.com/youtube/v3', endpoint]
-  if (key)
-    params.key = key
-  return [baseUrl.join('/'), queryString.stringify(params)].join('?')
-}
+    var baseUrl = ['https://www.googleapis.com/youtube/v3', endpoint];
+    if (key)
+        params.key = key;
+    return [baseUrl.join('/'), queryString.stringify(params)].join('?')
+};
 
 module.exports = function (youtubeApiKey) {
-  return function (endpoint, params, cb) {
-    request({
-      url: constructYoutubeApiUrl(youtubeApiKey, endpoint, params),
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      }
-    }, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        cb(null, JSON.parse(body));
-      } else {
-        cb(error)
-      }
-    });
-  }
-}
+    return function (endpoint, params, cb) {
+        request({
+            url    : constructYoutubeApiUrl(youtubeApiKey, endpoint, params),
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept'      : 'application/json'
+            }
+        }, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                cb(null, JSON.parse(body));
+            } else {
+                cb(error)
+            }
+        });
+    }
+};
